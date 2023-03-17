8f98fbc7ff1d43ccfd15c7d1c6939d1b3675e1de
IsaacFaf <2163607@cmontmorency.qc.ca>
2023-03-17 00:21:23 -0400

    UPDATE

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const boutonAlert= document.querySelector("h4");
-const para= document.querySelector("h4");
-console.log(document.querySelector("h4"))
-boutonAlert.addEventListener('click', function(){
-para.textContent = "Étudiant à Montmorency en Intégration Multimédia, il aime mettre des références d'animés dans ses projets dans le cadre de ses cours. <br></br> Dans le cadre du cours d'Optimisation Web enseigné par Marie-Michelle (superbe enseignante), il s'est jumelé avec Isaac pour produire un site Web de festival japonais. <br></br>  C'est alors qu'il a eu l'idée de ce projet. Ce projet consiste à remplacer les poissons d'un aquarium par des Katanas (arme légendaire). <br></br> Son projet a fait un succès internationnal et il est très reconnu pour ses idées aujourd'hui!";
-console.log("patateWorking");
-});
-
-//Pas de JS
